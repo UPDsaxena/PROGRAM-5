b6f48800238a6aa2dfe528c5ce047fef90a9a74e
user <user@DESKTOP-3C631U4>
2023-08-30 12:18:59 +0530

    Add project files.

---
+++ PROGRAM 5/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int start = 0;
+            int end = arr.Length - 1;
+            Console.WriteLine("Enter 5 Elements");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            while (start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+            Console.WriteLine("Reverse Array is :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
